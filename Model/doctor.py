3f169afff99bf706cc2b0e5347eb85d628de35e6
santiosoji2005 <santiosoji2005@gmail.com>
2025-11-18 01:13:01 -0500

    Update doctor.py

---
+++ Model/doctor.py
@@ -0,0 +1,5 @@
+class Doctor:
+    def __init__(self, doctor_name: str, speciality: str, dni: str):
+        self.doctor_name = doctor_name
+        self.speciality = speciality
+        self.dni = dni
